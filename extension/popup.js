084691604137c80e9a41c19f8da1bf327c677bda
yuvrajbhatkariya <manishbhatkariya2@gmail.com>
2026-07-01 21:00:38 +0530

    third commit

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://youtube-chatbot-using-langchain-dw3d.onrender.com";
 
 const YOUTUBE_VIDEO_PATTERN = /^https?:\/\/(www\.)?(youtube\.com\/watch\?.*v=|youtu\.be\/|youtube\.com\/shorts\/)/;
 
